bc720768f28e23be5f2eaf6768f0ff998fb5a2c5
Bolt-Of-Thunder <boltofthunder92@gmail.com>
2022-02-01 23:31:56 -0500

    Assessment Daniel Korol

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,80 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Daniel Korol"
 
-// We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0 // Totally total cookies
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
+const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbPlusBtn.addEventListener('click', function(e) {
+    gb = gb + 1;
+//    console.log('Gingerbread cookie count is: ', gb)
+    let gbTotalSum = document.querySelector('#qty-gb')
+    gbTotalSum.textContent = gb;
+    let totalSum = document.querySelector('#qty-total')
+    total++; totalSum.textContent = total;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+gbMinusBtn.addEventListener('click', function() {
+    if(gb > 0){
+        gb = gb - 1;
+        let gbTotalSum = document.querySelector('#qty-gb')
+        gbTotalSum.textContent = gb;
+//        console.log('Gingerbread cookie count is: ', gb)
+        let totalSum = document.querySelector('#qty-total')
+        total--; totalSum.textContent = total;
+    }
+})
+
+ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1;
+//    console.log('Chocolate Chip cookie count is: ', cc)
+    let ccTotalSum = document.querySelector('#qty-cc')
+    ccTotalSum.textContent = cc;
+    let totalSum = document.querySelector('#qty-total')
+    total++; totalSum.textContent = total;
+})
+
+ccMinusBtn.addEventListener('click', function() {
+    if(cc > 0){
+        cc = cc - 1;
+//        console.log('Chocolate Chip cookie count is: ', cc)
+        let ccTotalSum = document.querySelector('#qty-cc')
+        ccTotalSum.textContent = cc;
+        let totalSum = document.querySelector('#qty-total')
+        total--; totalSum.textContent = total;
+    }
+})
+
+sugarPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1;
+//    console.log('Sugar Sprinkle cookie count is: ', sugar)
+    let sugarTotalSum = document.querySelector('#qty-sugar')
+    sugarTotalSum.textContent = sugar;
+    let totalSum = document.querySelector('#qty-total')
+    total++; totalSum.textContent = total;
+})
+
+sugarMinusBtn.addEventListener('click', function() {
+    if(sugar > 0){
+        sugar = sugar - 1;
+//        console.log('Sugar Sprinkle cookie count is: ', sugar)
+        let sugarTotalSum = document.querySelector('#qty-sugar')
+        sugarTotalSum.textContent = sugar;
+        let totalSum = document.querySelector('#qty-total')
+        total--; totalSum.textContent = total;
+    }
+})
